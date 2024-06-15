8e58fa0f7f247c9caea28f87f8ac1f1266501ca6
Natnael <natyameha@gmail.com>
2024-06-15 10:33:13 +0300

    feat: update GraphQLConfig to include timeout for requests

--- lib/data/network/graphql_config.dart
+++ lib/data/network/graphql_config.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:ferry/ferry.dart';
 import 'package:ferry_hive_store/ferry_hive_store.dart';
 import 'package:get_it/get_it.dart';
+import 'package:gql_exec/gql_exec.dart';
 import 'package:gql_http_link/gql_http_link.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -10,18 +13,37 @@ class GraphQLConfig {
   static Client? _ferryGraphQlClient;
   static Future<Client> getFerryGraphQlClient() async {
     try {
-      if(_ferryGraphQlClient != null){
+      if (_ferryGraphQlClient != null) {
         return Future.value(_ferryGraphQlClient);
-      } 
+      }
       await Hive.initFlutter();
       final box = await Hive.openBox('graphql');
       final store = HiveStore(box);
       final cache = Cache(store: store);
-      final link = HttpLink('http://192.168.12.134:3000/graphql');
-      _ferryGraphQlClient = Client(link: link, cache: cache);
+      final link = HttpLink('http://192.168.1.4:3000/graphql');
+      final timeoutLink = TimeoutLink(
+          const Duration(seconds: 10), // Set the timeout duration here
+          link);
+      _ferryGraphQlClient = Client(link: timeoutLink, cache: cache);
       return _ferryGraphQlClient!;
     } catch (e) {
       return Future.error('error initializing graphql client');
     }
+  }
+}
+
+class TimeoutLink extends Link {
+  final Duration timeout;
+  final Link _link;
+
+  TimeoutLink(this.timeout, this._link);
+
+  @override
+  Stream<Response> request(Request request, [NextLink? forward]) {
+    final result = _link.request(request, forward);
+    return result.timeout(timeout, onTimeout: (sink) {
+      sink.addError(TimeoutException('Request timed out'));
+      sink.close();
+    });
   }
 }
